c121b38854f5b2105cb7f9b563ad1fb3ed9808aa
sergey-stack <Kasach.serzh@mail.ru>
2022-10-05 15:33:31 +0300

    Add animation in project

---
+++ InfoPlistFood/InfoPlistFood/Controllers/StViewController.swift
@@ -0,0 +1,57 @@
+//
+//  StViewController.swift
+//  InfoPlistFood
+//
+//  Created by сергей on 4.10.22.
+//
+
+import Firebase
+import UIKit
+
+class StViewController: UIViewController {
+    var imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+        imageView.image = UIImage(named: "Welcom")
+        return imageView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+        DispatchQueue.main.asyncAfter(deadline: .now()+5) {
+            self.performSegue(withIdentifier: "alreadyLoggedI", sender: self)
+        }
+        
+//        if Auth.auth().currentUser != nil {
+//            performSegue(withIdentifier: "alreadyLoggedIn", sender: nil)
+//        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.center = view.center
+        DispatchQueue.main.asyncAfter(deadline: .now()+3) {
+            self.animation()
+        }
+    }
+
+    func animation() {
+        UIView.animate(withDuration: 1) {
+            let size = self.view.frame.size.width * 2
+            let xpostion = size - self.view.frame.width
+            let ypostion = self.view.frame.height - size
+            self.imageView.frame = CGRect(x: -(xpostion/2), y: ypostion/2, width: size, height: size)
+            self.imageView.alpha = 0
+        }
+    }
+}
+        
+/*
+ // MARK: - Navigation
+
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+ }
+ */
--- InfoPlistFood/InfoPlistFood/Controllers/StartViewController.swift
+++ InfoPlistFood/InfoPlistFood/Controllers/StartViewController.swift
@@ -5,56 +5,25 @@
 //  Created by сергей on 2.10.22.
 //
 
-import UIKit
 import Firebase
+import UIKit
 
 class StartViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         if Auth.auth().currentUser != nil {
-          self.performSegue(withIdentifier: "alreadyLoggedIn", sender: nil)
-       }
-//        Auth.auth().addStateDidChangeListener { [weak self] _ , user in
-//                    guard let _ = user else {
-//                        return
-//                    }
-//                    self?.performSegue(withIdentifier: "goToMain", sender: nil)
-//                }
-//
-   }
-    
-    override func viewDidAppear(_ animated: Bool){
-     super.viewDidAppear(animated)
-     if Auth.auth().currentUser != nil {
-       self.performSegue(withIdentifier: "alreadyLoggedIn", sender: nil)
-    }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        print("I will appear")
+            self.performSegue(withIdentifier: "alreadyLoggedIn", sender: nil)
+        }
     }
 }
-        
-       
-    
-//    override func viewDidAppear(_ animated: Bool){
-//     super.viewDidAppear(animated)
-//     if Auth.auth().currentUser != nil {
-//       self.performSegue(withIdentifier: "alreadyLoggedIn", sender: nil)
-//    }
-//    }
-    
 
-    /*
-    // MARK: - Navigation
+/*
+ // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+ }
+ */
