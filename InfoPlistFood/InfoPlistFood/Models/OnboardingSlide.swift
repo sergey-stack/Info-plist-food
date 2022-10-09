d606f8fd776003f45961b54b4e2469eda69afe9e
sergey-stack <Kasach.serzh@mail.ru>
2022-10-09 19:03:41 +0300

    add onboarding

---
+++ InfoPlistFood/InfoPlistFood/Models/OnboardingSlide.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingSlide.swift
+//  InfoPlistFood
+//
+//  Created by сергей on 9.10.22.
+//
+
+import UIKit
+struct OnboardingSlide {
+    let title: String
+    let description: String
+    let image: UIImage
+}
+
